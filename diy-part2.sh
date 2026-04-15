69ff4d9ca660ca928751b76aefc4001e7916a130
a13156004291-glitch <a13156004291@gmail.com>
2026-04-16 01:56:40 +0800

    Update diy-part2.sh for AX3600 configuration
    Add configuration for AX3600 model and select plugins

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,16 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 强行勾选 AX3600 的机型（防止系统按默认 x86 编译）
+echo "CONFIG_TARGET_qualcommax=y" >> .config
+echo "CONFIG_TARGET_qualcommax_ipq807x=y" >> .config
+echo "CONFIG_TARGET_qualcommax_ipq807x_DEVICE_xiaomi_ax3600=y" >> .config
+
+# 强行勾选你要的插件（=y 就是你在 SSH 菜单里按空格打上的星号 <*>）
+echo "CONFIG_PACKAGE_luci-app-passwall=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-quickstart=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-istorex=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-zerotier=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
